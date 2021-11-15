d2e92d2d114901285d8248211dd84d2aa9ee469c
Phantom-liu <82393154+Phantom-liu@users.noreply.github.com>
2021-11-15 14:02:01 +0800

    汉诺塔

--- 动态规划DP/汉诺塔问题.cpp
+++ 动态规划DP/汉诺塔问题.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
 #include<string>
 using namespace std;
-void process(int N,string from,string to,string help)
+int process(int N,string from,string to,string help)
 {
 	if (N == 1) {
-		cout << "Move 1 from" << from << "to" << to << endl;
+		return 1;
+		//cout << "Move 1 from" << from << "to" << to << endl;
 	}
 	else {
-		process(N - 1, from, help,to);
-		cout << "Move "<<N<<" from" << from << "to" << to << endl;
-		process(N - 1, help,to, from);
+		//process(N - 1, from, help,to);
+		////cout << "Move "<<N<<" from" << from << "to" << to << endl;
+		//process(N - 1, help,to, from);
+		return process(N - 1, from, help, to) + 1 + process(N - 1, help, to, from);
 	}
 }
 int main()
 {
-	process(3, "×ó", "ÖÐ", "ÓÒ");
+	cout << process("s", "×ó", "ÖÐ", "ÓÒ");
 	return 0;
 }
